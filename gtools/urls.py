aa94a761aca77bb6c5edf59f09828f6cb7b77140
Charles van Niman <charles@phukish.com>
2016-08-05 16:09:45 -0500

    Adding initial framework.

---
+++ gtools/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls import include, url
+from django.contrib import admin
+
+urlpatterns = [
+    url(r'^op_webgui/', include('op_webgui.urls')),
+    url(r'^admin/', admin.site.urls),
+]
