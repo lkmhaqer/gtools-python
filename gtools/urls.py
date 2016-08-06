813bb30f504d6d19328cc38ef1c60e769ca2928d
Charles van Niman <charles@phukish.com>
2016-08-06 00:21:54 -0500

    Setting / route to op_webgui for now.

--- gtools/urls.py
+++ gtools/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'^/', include('op_webgui.urls')),
     url(r'^op_webgui/', include('op_webgui.urls')),
     url(r'^admin/', admin.site.urls),
 ]
