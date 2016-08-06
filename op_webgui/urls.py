9682bebf118d1619a13c25d1301e5250410500ee
Charles van Niman <charles@phukish.com>
2016-08-06 17:34:21 -0500

    Adding ASN list view, and Per-ASN neighbor list view.

--- op_webgui/urls.py
+++ op_webgui/urls.py
@@ -5,5 +5,7 @@ from . import views
 app_name = 'op_webgui'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^asn/$', views.asn, name='asn'),
+    url(r'^(?P<aut_num_id>[0-9]+)/aut_num/$', views.aut_num_detail, name='aut_num_detail'),
     url(r'^(?P<router_id>[0-9]+)/routers/$', views.router_detail, name='router_detail'),
 ]
--- op_webgui/views.py
+++ op_webgui/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import get_object_or_404, render
 
-from .models import router
+from .models import router, aut_num
 
 def index(request):
     router_list = router.objects.order_by('hostname')
-    context = {'router_list': router_list}
-    return render(request, 'op_webgui/index.html', context)
+    return render(request, 'op_webgui/index.html', {'router_list': router_list})
+
+def asn(request):
+    asn_list = aut_num.objects.order_by('asn')
+    return render(request, 'op_webgui/asns.html', {'asn_list': asn_list})
+
+def aut_num_detail(request, aut_num_id):
+    aut_num_obj = get_object_or_404(aut_num, pk=aut_num_id)
+    return render(request, 'op_webgui/aut_num.html', {'aut_num': aut_num_obj})
 
 def router_detail(request, router_id):
     router_obj = get_object_or_404(router, pk=router_id)
