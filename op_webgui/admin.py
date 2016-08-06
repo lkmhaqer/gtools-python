7e989b43a46e39fa6e634a4d3e42f899fcc4425c
Charles van Niman <charles@phukish.com>
2016-08-05 19:16:23 -0500

    Adding admin registration, and model __str__()

--- op_webgui/admin.py
+++ op_webgui/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import neighbor, router, network_os, aut_num
+
 # Register your models here.
+
+admin.site.register(neighbor)
+admin.site.register(router)
+admin.site.register(network_os)
+admin.site.register(aut_num)
--- op_webgui/models.py
+++ op_webgui/models.py
@@ -7,16 +7,28 @@ class neighbor(models.Model):
     peer_ip = models.GenericIPAddressField(unpack_ipv4=True)
     soft_inbound = models.BooleanField()
 
+    def __str__(self):
+        return self.peer_ip
+
 class router(models.Model):
     router_id = models.GenericIPAddressField(unpack_ipv4=True)
     hostname = models.CharField(max_length=255)
     ibgp = models.BooleanField()
     network_os = models.ForeignKey('network_os', on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.hostname
+
 class network_os(models.Model):
     name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
 
 class aut_num(models.Model):
     asn = models.IntegerField()
     name = models.CharField(max_length=255)
     contact = models.EmailField()
+
+    def __str__(self):
+        return self.asn + " - " + self.name
