e09e561a68f660b3b9508456c9a0149fcd7387cb
Charles van Niman <charles@phukish.com>
2018-05-02 18:14:29 -0500

    Adding interface model and cleanup of model file.

--- op_webgui/admin.py
+++ op_webgui/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import neighbor, router, network_os, aut_num
+from .models import interface, neighbor, router, network_os, aut_num
 
 # Register your models here.
 
@@ -8,3 +8,4 @@ admin.site.register(neighbor)
 admin.site.register(router)
 admin.site.register(network_os)
 admin.site.register(aut_num)
+admin.site.register(interface)
--- op_webgui/models.py
+++ op_webgui/models.py
@@ -2,20 +2,27 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+class interface(models.Model):
+    router    = models.ForeignKey('router', on_delete=models.CASCADE)
+    name      = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
 class neighbor(models.Model):
-    router = models.ForeignKey('router', on_delete=models.CASCADE)
-    aut_num = models.ForeignKey('aut_num', on_delete=models.CASCADE)
-    peer_ip = models.GenericIPAddressField(unpack_ipv4=True)
-    soft_inbound = models.BooleanField(default=1)
+    router        = models.ForeignKey('router', on_delete=models.CASCADE)
+    aut_num       = models.ForeignKey('aut_num', on_delete=models.CASCADE)
+    peer_ip       = models.GenericIPAddressField(unpack_ipv4=True)
+    soft_inbound  = models.BooleanField(default=1)
 
     def __str__(self):
         return self.aut_num.name + " - " + self.router.hostname + " " + self.peer_ip
 
 class router(models.Model):
-    routing_id = models.GenericIPAddressField(unpack_ipv4=True)
-    hostname = models.CharField(max_length=255)
-    ibgp = models.BooleanField()
-    network_os = models.ForeignKey('network_os', on_delete=models.CASCADE)
+    routing_id    = models.GenericIPAddressField(unpack_ipv4=True)
+    hostname      = models.CharField(max_length=255)
+    ibgp          = models.BooleanField()
+    network_os    = models.ForeignKey('network_os', on_delete=models.CASCADE)
 
     def __str__(self):
         return self.hostname
@@ -27,9 +34,9 @@ class network_os(models.Model):
         return self.name
 
 class aut_num(models.Model):
-    asn = models.BigIntegerField(unique=True)
-    name = models.CharField(max_length=255)
-    contact = models.EmailField(blank=True)
+    asn        = models.BigIntegerField(unique=True)
+    name       = models.CharField(max_length=255)
+    contact    = models.EmailField(blank=True)
 
     def __str__(self):
         return str(self.asn) + ": " + self.name + " (" + self.contact + ")"
