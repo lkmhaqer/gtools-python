91a03ffa3ae16fe80991748922b502e6985d8f0b
Charles van Niman <charles@phukish.com>
2018-05-10 12:01:11 -0500

    Adding static models to the admin interface.

--- op_webgui/admin.py
+++ op_webgui/admin.py
@@ -2,7 +2,7 @@
 
 from django.contrib import admin
 
-from .models import ipv6_address, ipv4_address, logical_interface, interface, neighbor, router, network_os, aut_num
+from .models import ipv6_static, ipv4_static, ipv6_address, ipv4_address, logical_interface, interface, neighbor, router, network_os, aut_num
 
 # Register your models here.
 
@@ -14,3 +14,5 @@ admin.site.register(interface)
 admin.site.register(logical_interface)
 admin.site.register(ipv4_address)
 admin.site.register(ipv6_address)
+admin.site.register(ipv4_static)
+admin.site.register(ipv6_static)
