d70be6e1156fb6147599cbee2a427316aa0c1a84
Charles van Niman <charles@phukish.com>
2018-05-12 17:53:29 -0500

    Cleaing up test segmentation, so base netdevice test has no static dependancy.

--- static/tests.py
+++ static/tests.py
@@ -4,13 +4,37 @@ from django.test import TestCase
 from django.urls import reverse
 
 from netdevice.tests import create_router
+from .models import ipv6_static, ipv4_static
+
+def create_v4_static(test_router, network):
+    v4_route     = ipv4_static.objects.create(router=test_router,
+                                              network=network,
+                                              cidr=16,
+                                              next_hop='172.16.0.1',
+                                             )
+
+    return v4_route
+
+def create_v6_static(test_router, network):
+    v6_route    = ipv6_static.objects.create(router=test_router,
+                                              network=network,
+                                              cidr=48,
+                                              next_hop='2001:db8::1',
+                                             )
+
+    return v6_route
+
+
 
 class StaticViewTest(TestCase):
-    def test_config_view_with_ios_static_route(self):
+    def test_config_view_with_a_single_ios_static_route(self):
         """
         Create an IOS router, interface, and IP addresses, then check the configuration template output.
         """
         test_router      = create_router('ios')
+        test_v4_route    = create_v4_static(test_router, '10.0.0.0')
+        test_v6_route    = create_v6_static(test_router, '2001:db8:100::')
+
         response         = self.client.get(reverse('op_webgui:router_config', kwargs={'router_id': test_router.id}))
 
         self.assertEqual(response.status_code, 200)
@@ -22,6 +46,9 @@ class StaticViewTest(TestCase):
         Create a JunOS router, interface, and IP addresses, then check the configuration template output.
         """
         test_router      = create_router('junos')
+        test_v4_route    = create_v4_static(test_router, '10.0.0.0')
+        test_v6_route    = create_v6_static(test_router, '2001:db8:100::')
+
         response         = self.client.get(reverse('op_webgui:router_config', kwargs={'router_id': test_router.id}))
 
         self.assertEqual(response.status_code, 200)
