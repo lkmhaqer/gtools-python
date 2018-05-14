010897bc256e23f7853a204172392599a2e93573
Charles van Niman <charles@phukish.com>
2018-05-13 22:06:33 -0500

    Adding multiple BGP neighbor JunOS and IOS tests.

--- bgp/tests.py
+++ bgp/tests.py
@@ -24,32 +24,72 @@ def create_neighbor(test_router, aut_num, peer_ip):
     return test_neighbor
 
 class AddressViewTests(TestCase):
-     def test_config_view_with_one_ios_neighbor(self):
-         """
-         Create a test IOS router, with one ipv4 and one ipv6 address then check config view.
-         """
-         test_router      = create_router('ios')
-         test_asn         = create_aut_num('65001')
-         test_v4_neighbor = create_neighbor(test_router, test_asn, '1.1.1.1')
-         test_v6_neighbor = create_neighbor(test_router, test_asn, '2001:db8::1')
+    def test_config_view_with_one_ios_neighbor(self):
+        """
+        Create a test IOS router, with one ipv4 and one ipv6 neighbors then check config view.
+        """
+        test_router      = create_router('ios')
+        test_asn         = create_aut_num('65001')
+        test_v4_neighbor = create_neighbor(test_router, test_asn, '1.1.1.1')
+        test_v6_neighbor = create_neighbor(test_router, test_asn, '2001:db8::1')
 
-         response = self.client.get(reverse('op_webgui:router_config', kwargs={'router_id': test_router.id}))
+        response = self.client.get(reverse('op_webgui:router_config', kwargs={'router_id': test_router.id}))
 
-         self.assertEqual(response.status_code, 200)
-         self.assertContains(response, 'neighbor 1.1.1.1 remote-as 65001')
-         self.assertContains(response, 'neighbor 2001:db8::1 remote-as 65001')
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'neighbor 1.1.1.1 remote-as 65001')
+        self.assertContains(response, 'neighbor 2001:db8::1 remote-as 65001')
 
-     def test_config_view_with_one_junos_neighbor(self):
-         """
-         Create a test JunOS router, with one ipv4 and one ipv6 address then check config view.
-         """
-         test_router     = create_router('junos')
-         test_asn         = create_aut_num('65001')
-         test_v4_neighbor = create_neighbor(test_router, test_asn, '1.1.1.1')
-         test_v6_neighbor = create_neighbor(test_router, test_asn, '2001:db8::1')
+    def test_config_view_with_one_junos_neighbor(self):
+        """
+        Create a test JunOS router, with one ipv4 and one ipv6 neighbors then check config view.
+        """
+        test_router      = create_router('junos')
+        test_asn         = create_aut_num('65001')
+        test_v4_neighbor = create_neighbor(test_router, test_asn, '1.1.1.1')
+        test_v6_neighbor = create_neighbor(test_router, test_asn, '2001:db8::1')
 
-         response = self.client.get(reverse('op_webgui:router_config', kwargs={'router_id': test_router.id}))
+        response = self.client.get(reverse('op_webgui:router_config', kwargs={'router_id': test_router.id}))
 
-         self.assertEqual(response.status_code, 200)
-         self.assertContains(response, 'peer-as 65001;\n            neighbor 1.1.1.1;')
-         self.assertContains(response, 'peer-as 65001;\n            neighbor 2001:db8::1;')
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'peer-as 65001;\n            neighbor 1.1.1.1;')
+        self.assertContains(response, 'peer-as 65001;\n            neighbor 2001:db8::1;')
+
+    def test_config_view_with_multiple_ios_neighbor(self):
+        """
+        Create a test IOS router, with 100 ipv4 and ipv6 neighbors then check the config view.
+        """
+        test_router      = create_router('ios')
+        test_asn         = create_aut_num('65001')
+        neighbor_count   = 100
+
+        for i in range(1, neighbor_count):
+            create_neighbor(test_router, test_asn, '1.1.' + str(i) + '.1')
+            create_neighbor(test_router, test_asn, '2001:db8:' +  str(i) + '::1')
+
+        response = self.client.get(reverse('op_webgui:router_config', kwargs={'router_id': test_router.id}))
+
+        self.assertEqual(response.status_code, 200)
+
+        for i in range(1, neighbor_count):
+            self.assertContains(response, 'neighbor 1.1.' + str(i) + '.1 remote-as 65001')
+            self.assertContains(response, 'neighbor 2001:db8:' +  str(i) + '::1 remote-as 65001')
+
+    def test_config_view_with_multiple_junos_neighbor(self):
+        """
+        Create a test IOS router, with 100 ipv4 and ipv6 neighbors then check the config view.
+        """
+        test_router      = create_router('junos')
+        test_asn         = create_aut_num('65001')
+        neighbor_count   = 100
+
+        for i in range(1, neighbor_count):
+            create_neighbor(test_router, test_asn, '1.1.' + str(i) + '.1')
+            create_neighbor(test_router, test_asn, '2001:db8:' +  str(i) + '::1')
+
+        response = self.client.get(reverse('op_webgui:router_config', kwargs={'router_id': test_router.id}))
+
+        self.assertEqual(response.status_code, 200)
+
+        for i in range(1, neighbor_count):
+            self.assertContains(response, 'peer-as 65001;\n            neighbor 1.1.' + str(i) + '.1;')
+            self.assertContains(response, 'peer-as 65001;\n            neighbor 2001:db8:' +  str(i) + '::1;')
