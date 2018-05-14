666648c803b7555558c320faa896c19fcae0719f
Charles van Niman <charles@phukish.com>
2018-05-14 11:13:16 -0500

    Move op_webgui tests over to proper folder.

--- netdevice/tests.py
+++ netdevice/tests.py
@@ -40,50 +40,6 @@ def create_interface(test_router):
     return test_logical_interface
 
 class NetdeviceViewTests(TestCase):
-    def test_index_view_with_no_routers(self):
-        """
-        We want to see an error if there are no routers.
-        """
-        response = self.client.get(reverse('op_webgui:index'))
-        self.assertEqual(response.status_code, 200)
-        self.assertContains(response, "No routers")
-        self.assertQuerysetEqual(response.context['router_list'], [])
-
-    def test_index_view_with_routers(self):
-        """
-        Create a router, then check that the view reflects this.
-        """
-        test_router      = create_router('ios')
-        response         = self.client.get(reverse('op_webgui:index'))
-
-        self.assertQuerysetEqual(
-            response.context['router_list'], ['<router: test-router>']
-        )
-
-    def test_index_view_with_many_routers(self):
-        """
-        Create a bunch of routers, then verify the view.
-        """
-        nos              = network_os.objects.create(name='many-test-os')
-        local_aut_num    = aut_num.objects.create(asn=65000, name='test asn')
-        new_router_list  = []
-
-        for i in range(0, 5):
-            router.objects.create(
-                hostname   = 'test-router-' + str(i),
-                routing_id = '1.1.1.' + str(i),
-                ibgp       = True,
-                network_os = nos,
-                local_aut_num=local_aut_num,
-            )
-            new_router_list.append('<router: test-router-' + str(i) + '>')
-
-        response = self.client.get(reverse('op_webgui:index'))
-
-        self.assertQuerysetEqual(
-            response.context['router_list'], new_router_list
-        )
-
     def test_config_view_with_ios_router(self):
         """
         Create an IOS router, interface, and IP addresses, then check the configuration template output.
--- op_webgui/tests.py
+++ op_webgui/tests.py
@@ -4,21 +4,59 @@ from django.test import TestCase
 from django.urls import reverse
 
 from netdevice.models import router, network_os
+from netdevice.tests import create_router
 from bgp.models import aut_num
 
 class InterfacesViewTests(TestCase):
+    def test_index_view_with_no_routers(self):
+        """
+        We want to see an error if there are no routers.
+        """
+        response = self.client.get(reverse('op_webgui:index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "No routers")
+        self.assertQuerysetEqual(response.context['router_list'], [])
+
+    def test_index_view_with_routers(self):
+        """
+        Create a router, then check that the view reflects this.
+        """
+        test_router      = create_router('ios')
+        response         = self.client.get(reverse('op_webgui:index'))
+
+        self.assertQuerysetEqual(
+            response.context['router_list'], ['<router: test-router>']
+        )
+
+    def test_index_view_with_many_routers(self):
+        """
+        Create a bunch of routers, then verify the view.
+        """
+        nos              = network_os.objects.create(name='many-test-os')
+        local_aut_num    = aut_num.objects.create(asn=65000, name='test asn')
+        new_router_list  = []
+
+        for i in range(0, 5):
+            router.objects.create(
+                hostname   = 'test-router-' + str(i),
+                routing_id = '1.1.1.' + str(i),
+                ibgp       = True,
+                network_os = nos,
+                local_aut_num=local_aut_num,
+            )
+            new_router_list.append('<router: test-router-' + str(i) + '>')
+
+        response = self.client.get(reverse('op_webgui:index'))
+
+        self.assertQuerysetEqual(
+            response.context['router_list'], new_router_list
+        )
+
     def test_router_with_no_intefaces(self):
         """
         Show no results if there are no interfaces.
         """
-        nos              = network_os.objects.create(name='test-os')
-        local_aut_num    = aut_num.objects.create(asn=65000, name='Test ASN')
-        one_router       = router.objects.create(routing_id='1.1.1.1',
-                                                 hostname='test-router',
-                                                 ibgp=True,
-                                                 network_os=nos,
-                                                 local_aut_num=local_aut_num
-                                                )
+        one_router       = create_router('junos')
 
         response         = self.client.get(reverse('op_webgui:router_detail',
                                            kwargs={'router_id': one_router.id})
