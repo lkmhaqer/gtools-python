03311018cf16ea0150f62b50edd9c424b3f95221
Charles van Niman <charles@phukish.com>
2018-05-16 22:29:51 -0500

    Add static route buttons for add/edit v4 and v6. Fix #14

--- op_webgui/forms.py
+++ op_webgui/forms.py
@@ -3,28 +3,29 @@
 from django import forms
 
 from netdevice.models import router
+from static.models import ipv6_static, ipv4_static
 from bgp.models import aut_num
 
 class RouterForm(forms.ModelForm):
 
     class Meta:
         model  = router
-        fields = (
-                  'routing_id',
-                  'hostname',
-                  'network_os',
-                  'local_aut_num',
-                  'ibgp',
-                  'service_ssh',
-                  'service_netconf',
-                 )
+        fields = ('__all__')
 
 class ASNForm(forms.ModelForm):
 
     class Meta:
         model  = aut_num
-        fields = (
-                  'asn',
-                  'name',
-                  'contact',
-                 )
+        fields = ('__all__')
+
+class IPv6StaticForm(forms.ModelForm):
+
+    class Meta:
+        model  = ipv6_static
+        fields = ('__all__')
+
+class IPv4StaticForm(forms.ModelForm):
+
+    class Meta:
+        model  = ipv4_static
+        fields = ('__all__')
--- op_webgui/views.py
+++ op_webgui/views.py
@@ -5,9 +5,10 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import get_object_or_404, render, redirect
 
 from netdevice.models import router
+from static.models import ipv6_static, ipv4_static
 from bgp.models import aut_num
 
-from .forms import RouterForm, ASNForm
+from .forms import RouterForm, ASNForm, IPv6StaticForm, IPv4StaticForm
 
 @login_required
 def index(request):
@@ -66,6 +67,54 @@ def aut_num_edit(request, aut_num_id):
             return redirect('op_webgui:aut_num_detail', aut_num_id=asn.pk)
     else:
         form = ASNForm(instance=asn)
+    return render(request, 'op_webgui/generic_edit.html', {'form': form})
+
+@login_required
+def ipv6_static_create(request, router_id):
+    router_obj = get_object_or_404(router, pk=router_id)
+    if request.method == "POST":
+        form = IPv6StaticForm(request.POST)
+        if form.is_valid():
+            ipv6_static_obj = form.save()
+            return redirect('op_webgui:router_detail', router_id=ipv6_static_obj.router.pk)
+    else:
+        form = IPv6StaticForm(initial={'router': router_obj})
+    return render(request, 'op_webgui/generic_edit.html', {'form': form})
+
+@login_required
+def ipv6_static_edit(request, ipv6_static_id):
+    ipv6_static_obj = get_object_or_404(ipv6_static, pk=ipv6_static_id)
+    if request.method == "POST":
+        form = IPv6StaticForm(request.POST, instance=ipv6_static_obj)
+        if form.is_valid():
+            ipv6_static_obj = form.save()
+            return redirect('op_webgui:router_detail', router_id=ipv6_static_obj.router.pk)
+    else:
+        form = IPv6StaticForm(instance=ipv6_static_obj)
+    return render(request, 'op_webgui/generic_edit.html', {'form': form})
+
+@login_required
+def ipv4_static_create(request, router_id):
+    router_obj = get_object_or_404(router, pk=router_id)
+    if request.method == "POST":
+        form = IPv4StaticForm(request.POST)
+        if form.is_valid():
+            ipv4_static_obj = form.save()
+            return redirect('op_webgui:router_detail', router_id=ipv4_static_obj.router.pk)
+    else:
+        form = IPv4StaticForm(initial={'router': router_obj})
+    return render(request, 'op_webgui/generic_edit.html', {'form': form})
+
+@login_required
+def ipv4_static_edit(request, ipv4_static_id):
+    ipv4_static_obj = get_object_or_404(ipv4_static, pk=ipv4_static_id)
+    if request.method == "POST":
+        form = IPv4StaticForm(request.POST, instance=ipv4_static_obj)
+        if form.is_valid():
+            ipv4_static_obj = form.save()
+            return redirect('op_webgui:router_detail', router_id=ipv4_static_obj.router.pk)
+    else:
+        form = IPv4StaticForm(instance=ipv4_static_obj)
     return render(request, 'op_webgui/generic_edit.html', {'form': form})
 
 @login_required
