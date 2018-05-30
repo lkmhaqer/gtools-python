35d6b6dd8459be47be4e438086b562bc1daf4e06
Charles van Niman <charles@phukish.com>
2018-05-29 22:59:34 -0500

    Update settings for assets dir.

--- gtools/settings.py
+++ gtools/settings.py
@@ -128,8 +128,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-STATIC_URL  = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
+STATIC_URL  = '/assets/'
 
 # User-set project settings
 #
