6e3c8d618b7ae48b75232230294d602ddce5cc0b
Charles van Niman <charles@phukish.com>
2018-05-29 23:22:10 -0500

    Update gtools/settings.py with all the vars needed to serve our static assets/css/dashboard.css

--- gtools/settings.py
+++ gtools/settings.py
@@ -129,7 +129,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL  = '/static/'
-STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR,'assets'))
+STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, 'files'))
+STATICFILES_DIRS = (os.path.join(BASE_DIR,'assets'),)
 
 # User-set project settings
 #
