4eee509fff9141eb9d7b08b198eaf6cab86dc00a
Charles van Niman <charles@phukish.com>
2018-06-10 00:57:09 -0500

    Update bgp/models.py, bgp/tests.py, for 80col.

--- bgp/models.py
+++ bgp/models.py
@@ -9,31 +9,43 @@ class neighbor(models.Model):
 
     router:          Router that the peer session is tied to.
     aut_num:         ASN that the peer session is tied to.
-    peer_ip:         IP Address of BGP neighbor.                                                          [203.0.113.1]
-    soft_inbound:    Boolean value for if soft-reconfiguration inbound is enabled on the peer session.    [True]
+    peer_ip:         IP Address of BGP neighbor.
+                     [203.0.113.1]
+    soft_inbound:    Boolean value for if soft-reconfiguration inbound is
+                     enabled on the peer session.
+                     [True]
 
     Example string: PeeringBuddy - Router1 203.0.113.1
     """
 
-    router        = models.ForeignKey('netdevice.router', on_delete=models.CASCADE)
+    router        = models.ForeignKey(
+                                      'netdevice.router',
+                                      on_delete=models.CASCADE
+                                     )
     aut_num       = models.ForeignKey('aut_num', on_delete=models.CASCADE)
     peer_ip       = models.GenericIPAddressField(unpack_ipv4=True)
     soft_inbound  = models.BooleanField(default=True)
 
     def __str__(self):
-        return self.aut_num.name + " - " + self.router.hostname + " " + self.peer_ip
+        asn_router = self.aut_num.name + " - " + self.router.hostname + " "
+        return asn_router + self.peer_ip
 
 
 
 class aut_num(models.Model):
     """
-    Autonomous System Number - Used in BGPv4 to represent an administrative domain where internet prefixes are exchanged.
+    Autonomous System Number - Used in BGPv4 to represent an
+                               administrative domain where internet prefixes
+                               are exchanged.
 
-    asn:        The actual autonomous system number of someone we neighbor.    [65000]
-    name:       A specific company or personal name for the peer.              [NTT Communications]
-    contact:    An email address to go along with the name.                    [john.smith@example.com]
+    asn:        The actual autonomous system number of someone we neighbor.
+                [65000]
+    name:       A specific company or personal name for the peer.
+                [123 ISP Inc.]
+    contact:    An email address to go along with the name.
+                [john.smith@example.com]
 
-    Example string: 65000: NTT Communications (john.smith@example.com)
+    Example string: 65000: 123 ISP Inc. (john.smith@example.com)
     """
 
     asn        = models.BigIntegerField(unique=True)
--- bgp/tests.py
+++ bgp/tests.py
@@ -26,18 +26,28 @@ def create_neighbor(test_router, aut_num, peer_ip):
 
 class AddressViewTests(TestCase):
     def setUp(self):
-        self.client.force_login(User.objects.get_or_create(username='testuser')[0])
+        test_user = User.objects.get_or_create(username='testuser')[0]
+        self.client.force_login(test_user)
 
     def test_config_view_with_one_ios_neighbor(self):
         """
-        Create a test IOS router, with one ipv4 and one ipv6 neighbors then check config view.
+        Create a test IOS router, with one ipv4 and one ipv6 neighbors
+        then check config view.
         """
         test_router      = create_router('ios')
         test_asn         = create_aut_num('65001')
         test_v4_neighbor = create_neighbor(test_router, test_asn, '1.1.1.1')
-        test_v6_neighbor = create_neighbor(test_router, test_asn, '2001:db8::1')
+        test_v6_neighbor = create_neighbor(
+                                           test_router,
+                                           test_asn,
+                                           '2001:db8::1'
+                                          )
+        url              = reverse(
+                                   'op_webgui:router_config',
+                                   kwargs={'router_id': test_router.id},
+                                  )
 
-        response = self.client.get(reverse('op_webgui:router_config', kwargs={'router_id': test_router.id}))
+        response = self.client.get(url)
 
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, 'neighbor 1.1.1.1 remote-as 65001')
@@ -45,14 +55,19 @@ class AddressViewTests(TestCase):
 
     def test_config_view_with_one_junos_neighbor(self):
         """
-        Create a test JunOS router, with one ipv4 and one ipv6 neighbors then check config view.
+        Create a test JunOS router, with one ipv4 and one ipv6 neighbors
+        then check config view.
         """
         test_router      = create_router('junos')
         test_asn         = create_aut_num('65001')
         test_v4_neighbor = create_neighbor(test_router, test_asn, '1.1.1.1')
         test_v6_neighbor = create_neighbor(test_router, test_asn, '2001:db8::1')
+        url              = reverse(
+                                   'op_webgui:router_config',
+                                   kwargs={'router_id': test_router.id},
+                                  )
 
-        response = self.client.get(reverse('op_webgui:router_config', kwargs={'router_id': test_router.id}))
+        response = self.client.get(url)
 
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, 'peer-as 65001;')
@@ -61,7 +76,8 @@ class AddressViewTests(TestCase):
 
     def test_config_view_with_multiple_ios_neighbor(self):
         """
-        Create a test IOS router, with 100 ipv4 and ipv6 neighbors then check the config view.
+        Create a test IOS router, with 100 ipv4 and ipv6 neighbors
+        then check the config view.
         """
         test_router      = create_router('ios')
         test_asn         = create_aut_num('65001')
@@ -69,24 +85,36 @@ class AddressViewTests(TestCase):
 
         for i in range(1, neighbor_count):
             create_neighbor(test_router, test_asn, '1.1.' + str(i) + '.1')
-            create_neighbor(test_router, test_asn, '2001:db8:' +  str(i) + '::1')
+            create_neighbor(
+                            test_router,
+                            test_asn,
+                            '2001:db8:' +  str(i) + '::1'
+                           )
 
-        response = self.client.get(reverse('op_webgui:router_config', kwargs={'router_id': test_router.id}))
+        url = reverse(
+                      'op_webgui:router_config',
+                      kwargs={'router_id': test_router.id},
+                     )
+
+        response = self.client.get(url)
 
         self.assertEqual(response.status_code, 200)
 
         for i in range(1, neighbor_count):
-            self.assertContains(response, 'neighbor 1.1.' + str(i) + '.1 remote-as 65001')
-            self.assertContains(response, 'neighbor 2001:db8:' +  str(i) + '::1 remote-as 65001')
+            v4_text = 'neighbor 1.1.' + str(i) + '.1 remote-as 65001'
+            v6_text = 'neighbor 2001:db8:' +  str(i) + '::1 remote-as 65001'
+            self.assertContains(response, v4_text)
+            self.assertContains(response, v6_text)
 
     def test_config_view_with_multiple_junos_neighbor(self):
         """
-        Create a test IOS router, with 100 ipv4 and ipv6 neighbors then check the config view.
+        Create a test IOS router, with 100 ipv4 and ipv6 neighbors
+        then check the config view.
         """
         test_router      = create_router('junos')
         test_asn_one     = create_aut_num('65001')
         test_asn_two     = create_aut_num('65002')
-        neighbor_count   = 50 # Set this to half desired value, as we'll run two peer-groups.
+        neighbor_count   = 50
 
         asn_one_config   = '        group test-asn {\n'
         asn_one_config  += '            type external;\n'
@@ -104,16 +132,30 @@ class AddressViewTests(TestCase):
             asn_two_config += '            neighbor 2.2.' + str(i) + '.1; \n'
 
         for i in range(1, neighbor_count):
-            create_neighbor(test_router, test_asn_one, '2001:db8:' +  str(i) + '::1')
-            asn_one_config += '            neighbor 2001:db8:' +  str(i) + '::1; \n'
+            create_neighbor(
+                            test_router,
+                            test_asn_one,
+                            '2001:db8:' +  str(i) + '::1'
+                           )
+            asn_one_config += '            neighbor 2001:db8:'
+            asn_one_config += str(i) + '::1; \n'
 
-            create_neighbor(test_router, test_asn_two, '2001:db8:' +  str(i) + ':2::1')
-            asn_two_config += '            neighbor 2001:db8:' +  str(i) + ':2::1; \n'
+            create_neighbor(
+                            test_router,
+                            test_asn_two,
+                            '2001:db8:' +  str(i) + ':2::1'
+                           )
+            asn_two_config += '            neighbor 2001:db8:'
+            asn_two_config += str(i) + ':2::1; \n'
 
         asn_one_config += '        }'
         asn_two_config += '        }'
+        url             = reverse(
+                                  'op_webgui:router_config',
+                                  kwargs={'router_id': test_router.id},
+                                 )
 
-        response = self.client.get(reverse('op_webgui:router_config', kwargs={'router_id': test_router.id}))
+        response = self.client.get(url)
 
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, asn_one_config)
@@ -123,21 +165,34 @@ class AddressViewTests(TestCase):
         """
         Create a test router, and then test the form view of create display.
         """
-        test_router     = create_router('junos')
+        test_router  = create_router('junos')
+        url          = reverse(
+                               'op_webgui:neighbor_create',
+                               kwargs={'router_id': test_router.id},
+                              )
 
-        response = self.client.get(reverse('op_webgui:neighbor_create', kwargs={'router_id': test_router.id}))
+        response = self.client.get(url)
 
         self.assertEqual(response.status_code, 200)
 
     def test_edit_neighbor_form_view(self):
         """
-        Create a test router, and neighbor, then test the form view of neighbor edit is displayed correctly.
+        Create a test router, and neighbor, then test the form view of neighbor
+        edit is displayed correctly.
         """
         test_router     = create_router('junos')
         test_asn_one    = create_aut_num('65001')
-        test_neighbor   = create_neighbor(test_router, test_asn_one, '2001:db8:1::1')
+        test_neighbor   = create_neighbor(
+                                          test_router,
+                                          test_asn_one,
+                                          '2001:db8:1::1'
+                                         )
+        url             = reverse(
+                                  'op_webgui:neighbor_edit',
+                                  kwargs={'neighbor_id': test_neighbor.id},
+                                 )
 
-        response = self.client.get(reverse('op_webgui:neighbor_edit', kwargs={'neighbor_id': test_neighbor.id}))
+        response = self.client.get(url)
 
         self.assertEqual(response.status_code, 200)
 
@@ -154,7 +209,11 @@ class AddressViewTests(TestCase):
         Create an ASN, then test the form view of the edit ASN display.
         """
         test_asn = create_aut_num('65001')
+        url      = reverse(
+                           'op_webgui:aut_num_edit',
+                           kwargs={'aut_num_id': test_asn.id},
+                          )
 
-        response = self.client.get(reverse('op_webgui:aut_num_edit', kwargs={'aut_num_id': test_asn.id}))
+        response = self.client.get(url)
 
         self.assertEqual(response.status_code, 200)
